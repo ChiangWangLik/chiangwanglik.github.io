3ac55b613493cd39a6160a09181023fde7d13787
ChiangWangLik <u3568294@connect.hku.hk>
2023-09-26 14:07:24 +0800

    Updates

---
+++ precache-manifest.917e56911f631118adec57a3f8a2d083.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0771229d23006a02b7fb",
+    "url": "./static/css/main.abe077e8.chunk.css"
+  },
+  {
+    "revision": "0771229d23006a02b7fb",
+    "url": "./static/js/main.19c4acca.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "a00d3fa62ec1f7a60d64",
+    "url": "./static/js/2.18d2d9f8.chunk.js"
+  },
+  {
+    "revision": "73157a731440e713f3786dbad072d448",
+    "url": "./index.html"
+  }
+];
