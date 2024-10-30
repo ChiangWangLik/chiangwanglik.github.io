91845a9d01d79f06dd247910c0862f2cbd284f75
ChiangWangLik <u3568294@connect.hku.hk>
2024-10-30 18:44:07 +0800

    experience update

--- src/components/pages/Experience.js
+++ src/components/pages/Experience.js
@@ -3,6 +3,47 @@ import "../../App.css";
 import Footer from "../javascripts/Footer";
 
 export default function Experience() {
+  const experiences = [
+    {
+      datetime: "Oct 2023 - Sept 2024",
+      company: "Judiciary",
+      position: "Contract Junior Programmer",
+      description: "- Assist the Judiciary in implementing systems under IT Strategy Plan\n" + 
+      "- Develop program to help with the case filing and managing for judges."
+    },
+    {
+      datetime: "Jul 2023 - Aug 2023",
+      company: "GNS Technology Holdings Limited",
+      position: "Web Application Developer",
+      description: 'Implement program for the illumination of the Light Rail.'
+    },
+    {
+      datetime: "Nov 2022 - Dec 2022",
+      company: "Centre for the Enhancement of Teaching and Learning",
+      position: "Student Research Assistant",
+      description: "Interview students on their effectiveness on learning, collect and verify" + 
+      " data obtained, analyze the learning performance of students."
+    },
+    {
+      datetime: "Jun 2022 - Aug 2022",
+      company: "Biomed Technology Holdings Limited",
+      position: "Information Technology Assistant",
+      description: "- Website building: WordPress (Elementor), Vue.js, Docker\n" + 
+      "Build a website from scratch to deliver information and courses offered\n" + 
+      "- Mobile app creation: Flutter, Dart\n" + 
+      "Frontend development to display information about the products.\n" + 
+      "- Leaflets design: Canva, Visme\n" + 
+      "Design leaflets for propaganda and website displaying."
+    },
+    {
+      datetime: "Feb 2020 - May 2021",
+      company: "JSF Team",
+      position: "Customer Service Assistant",
+      description: "Communicate with customers on product purchasing, update social media" + 
+      " regularly to attract potential customers, pack products ready for delivery."
+    }
+  ];
+
   return (
     <>
       <div className="experience">
@@ -10,63 +51,15 @@ export default function Experience() {
         <div className="box">
           <h1>Experience</h1>
           <hr className="rounded" />
-
-          <h3 className="datetime">Oct 2023 - Present</h3>
-          <h3 className="company">
-            Department of Justice
-          </h3>
-          <p className="position">Junior Programmer</p>
-          <p className="description">
-            - Assist the Judiciary in implementing systems under IT Strategy Plan
-            <br />
-            <br />
-            - Develop program to help with the case filing and managing for judges
-          </p>
-
-          <hr className="rounded" />
-
-          <h3 className="datetime">Nov 2022 - Dec 2022</h3>
-          <h3 className="company">
-            Centre for the Enhancement of Teaching and Learning
-          </h3>
-          <p className="position">Student Research Assistant</p>
-          <p className="description">
-            Interview students on their effectiveness on learning, collect and
-            verify data obtained, analyze the learning performance of students.
-          </p>
-
-          <hr className="rounded" />
-
-          <h3 className="datetime">Jun 2022 - Aug 2022</h3>
-          <h3 className="company">Biomed Technology Holdings Limited</h3>
-          <p className="position">Information Technology Assistant</p>
-          <p className="description">
-            - Website building: WordPress (Elementor), Vue.js, Docker
-            <br />
-            Build a website from scratch to deliver information and courses
-            offered
-            <br />
-            <br />
-            - Mobile app creation: Flutter, Dart
-            <br />
-            Frontend development to display information about the products.
-            <br />
-            <br />
-            - Leaflets design: Canva, Visme
-            <br />
-            Design leaflets for propaganda and website displaying.
-          </p>
-
-          <hr className="rounded" />
-
-          <h3 className="datetime">Feb 2020 - May 2021</h3>
-          <h3 className="company">JSF Team</h3>
-          <p className="position">Customer Service Assistant</p>
-          <p className="description">
-            Communicate with customers on product purchasing, update social media
-            regularly to attract potential customers, pack products ready for
-            delivery.
-          </p>
+          {experiences.map((exp) => (
+            <>
+              <h3 className="datetime">{exp.datetime}</h3>
+              <h3 className="company">{exp.company}</h3>
+              <p className="position">{exp.position}</p>
+              <p className="description">{exp.description.split('\n').map(str => <p>{str}</p>)}</p>
+              <hr className="rounded" />
+            </>
+          ))}
         </div>
         <Footer />
       </div>
